87e889648b7e3f01c6be09c9ec547e4a4c08e7ce
Cameron Battagler <fictoris@gmail.com>
2022-01-21 16:09:43 -0600

    using class to pass octoprint settings

--- octoprint_iothub.py
+++ octoprint_iothub.py
@@ -11,21 +11,25 @@ import requests, json
 from azure.iot.device.aio import IoTHubDeviceClient
 from azure.iot.device import Message
 
-octoprint_url = ""
-octoprint_api_key = ""
+class octoprint_settings:
+    def __init__(self, octoprint_url, octoprint_api_key):
+        self.octoprint_url = octoprint_url
+        self.octoprint_api_key = octoprint_api_key
+
+ops = octoprint_settings("", "")
 
 
-async def send_recurring_telemetry(device_client, octoprint_url, octoprint_api_key):
+async def send_recurring_telemetry(device_client):
     # Connect the client.
     await device_client.connect()
 
     # Send recurring telemetry
     i = 0
     while True:
-        header = {'X-Api-Key': octoprint_api_key}
-        r = requests.get(octoprint_url + '/api/printer', headers=header)
+        header = {'X-Api-Key': ops.octoprint_api_key}
+        r = requests.get(ops.octoprint_url + '/api/printer', headers=header)
         printer = json.loads(r.text)
-        r = requests.get(octoprint_url + '/api/job', headers=header)
+        r = requests.get(ops.octoprint_url + '/api/job', headers=header)
         job = json.loads(r.text)
         octoprint = {**printer, **job}
 
@@ -40,29 +44,32 @@ async def send_recurring_telemetry(device_client, octoprint_url, octoprint_api_k
         await device_client.patch_twin_reported_properties(printer["temperature"])
         time.sleep(2)
 
+async def message_received_handler(message):
+    data = json.loads(message.data)
+    if "lcdMessage" in data:
+        commands = {
+            "commands": ["M117 " + data["lcdMessage"]]
+        }
+        header = {'X-Api-Key': ops.octoprint_api_key}
+        r = requests.get(ops.octoprint_url + '/api/printer/command', data=commands, headers=header)
+    print("the data in the message received was ")
+    print(message.data)
+    print("custom properties are")
+    print(message.custom_properties)
+    print("content Type: {0}".format(message.content_type))
+    print("")
+
+async def twin_patch_handler(patch):        
+    print("the data in the desired properties patch was: {}".format(patch))
+
 def main():
     # The connection string for a device should never be stored in code. For the sake of simplicity we're using an environment variable here.
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
     octoprint_url = os.getenv("OCTOPRINT_URL").strip('/')
     octoprint_api_key = os.getenv("OCTOPRINT_API_KEY")
 
-    def message_received_handler(message):
-        data = json.loads(message.data)
-        if "lcdMessage" in data:
-            commands = {
-                "commands": ["M117 " + data["lcdMessage"]]
-            }
-            header = {'X-Api-Key': octoprint_api_key}
-            r = requests.get(octoprint_url + '/api/printer/command', data=commands, headers=header)
-        print("the data in the message received was ")
-        print(message.data)
-        print("custom properties are")
-        print(message.custom_properties)
-        print("content Type: {0}".format(message.content_type))
-        print("")
-
-    def twin_patch_handler(patch):        
-        print("the data in the desired properties patch was: {}".format(patch))
+    ops.octoprint_api_key = octoprint_api_key
+    ops.octoprint_url = octoprint_url
 
     # The client object is used to interact with your Azure IoT hub.
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
